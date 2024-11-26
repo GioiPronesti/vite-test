c20378b37ef45d03e48726337a92b449e329c40d
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-26 19:01:53 +0100

    add: function App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 
 import './App.css'
+import { tasks } from './tasks'
+
+
 
 function App() {
 
+const currentArray = tasks.filter((el) =>  {
+  el.state === "backlog" || el.state === "in_progress"
+})
+  
+  
+
   return (
-    <h1>Hello Word !</h1>
+    <>
+      <div>
+        <header className={"header container"}>
+          <h1>Task Manager</h1>
+        </header>
+        <main className={"main container"}>
+          <h2>Current Task ({currentArray.length})</h2>
+        <ul>
+          <li>{currentArray}</li>
+        </ul>
+        <hr></hr>
+          <h2>Completed Task </h2>
+        </main>
+      </div>
+    </>
+     
   )
 }
 
